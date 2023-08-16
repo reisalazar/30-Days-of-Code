34c2630c5296e1d2ff30ed60731e70fa07bcd0c3
unknown <rrsb45@gmail.com>
2023-08-16 00:00:56 -0300

    Day 11: 2D Arrays

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -1,8 +1,26 @@
-fun main(args: Array<String>) {
+import kotlin.math.max
 
-    val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
+fun main(args: Array<String>) {
+    val arr = Array(6) { Array(6) { 0 } }
 
     for (i in 0 until 6) {
-        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+        arr[i] = readLine()!!.trimEnd().split(" ")
+            .map { it.toInt() }.toTypedArray()
     }
+
+    var maxHourGlass = Int.MIN_VALUE
+    for (i in 0..3) {
+        for (j in 0..3) {
+            maxHourGlass =
+                max( 
+                    arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                            + arr[i + 1][j + 1]
+                            + arr[i + 2][j] + arr[i + 2][j + 1]
+                            + arr[i + 2][j + 2],
+                    maxHourGlass
+                )
+        }
+    }
+
+    println(maxHourGlass)
 }
