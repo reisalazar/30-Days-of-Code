3cef9df9b745854ed4195308bb31af445462b8f9
unknown <rrsb45@gmail.com>
2023-08-17 18:32:06 -0300

    Day 14: Scope

---
+++ src/main/kotlin/Solution2.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    // Add your code here
+    Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    public void computeDifference() {
+        int[] array = Arrays.stream(elements).sorted().toArray();
+        int max = array[array.length - 1];
+        int min = array[0];
+        maximumDifference = max - min;
+    }
+}
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
